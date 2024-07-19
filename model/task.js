3de339f08f5d76e3d2f4bc51437306cbb185a43d
kavyadadhich1 <138555154+kavya0dadhich@users.noreply.github.com>
2024-07-19 11:37:50 +0530

    update

--- model/task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/TIME_TODO",console.log("Data base is connect.."));
-const TASK_MODUL =  new mongoose.Schema({
-    task:String,
-    time:String,
-    sendMSG:Boolean,
-})
-module.exports = mongoose.model("task",TASK_MODUL);
